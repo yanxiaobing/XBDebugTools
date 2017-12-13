f1c1f81feab300508b275465c583fbabeda9a450
XBingo <dove025@qq.com>
2017-12-13 23:01:34 +0800

    创建项目

---
+++ XBExceptionHandler/XBExceptionInfo.m
@@ -0,0 +1,13 @@
+//
+//  XBExceptionInfo.m
+//  XBExceptionHandler
+//
+//  Created by XBingo on 2017/12/13.
+//  Copyright © 2017年 XBingo. All rights reserved.
+//
+
+#import "XBExceptionInfo.h"
+
+@implementation XBExceptionInfo
+
+@end
---
+++ XBExceptionHandler/XBExceptionViewController.h
@@ -0,0 +1,15 @@
+//
+//  XBExceptionViewController.h
+//  XBExceptionHandler
+//
+//  Created by XBingo on 2017/12/13.
+//  Copyright © 2017年 XBingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XBExceptionViewController : UIViewController
+
++(void)present;
+
+@end
